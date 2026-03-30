af7c6ecd0c4711ccdbf9f7b73cf1e02a5531543e
Earl Justine Zafra <totszafra21@gmail.com>
2026-03-30 23:16:26 +0800

    fix: hardcode supabase credentials

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://vaxszpsdzbdmzzfixfzk.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZheHN6cHNkemJkbXp6Zml4ZnprIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ4NTY2NDgsImV4cCI6MjA5MDQzMjY0OH0.j8_pjV3VLBaiDUcb9b2tUl1X1P9WcJ7yzidv-7e1XoY";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
